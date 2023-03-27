50b003c33c138cb79e80470a3cd9bf94ac263bb2
Cornelia <cornelia@students.amikom.ac.id>
2023-03-27 22:55:49 +0700

    first commit

---
+++ LatihanClassDanObject/Mobil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    public class Mobil
+    {
+        // properties
+        public string Warna { get; set; }
+        public string JumlahPintu { get; set; }
+        public string Merek { get; set; }
+        public string Model { get; set; }
+        public string TahunKeluaran { get; set; }
+
+
+        // method
+        public void Input()
+        {
+            Console.WriteLine("Warna {0}", Warna);
+            Console.WriteLine("Jumlah Pintu {0}", JumlahPintu);
+            Console.WriteLine("Merek {0}", Merek);
+            Console.WriteLine("Model {0}", Model);
+            Console.WriteLine("Tahun Keluaran {0}", TahunKeluaran);
+            Console.WriteLine("Mobil Toyota berjalan dengan kecepatan 160km/jam");
+
+        }
+        public void Klakson()
+        {
+            Console.WriteLine("Tin... Tin... Tin...");
+        }
+            public void Info()
+        { 
+            Console.WriteLine("Mobil saya berwarna Merah, merek Toyota, model MVP, tahun keluaran 2022, dengan jumlah pintu sebanyak Empat");
+        }
+
+
+    }
+}
---
+++ LatihanClassDanObject/Program.cs
@@ -0,0 +1,28 @@
+﻿using LatihanClassDanObject;
+
+namespace classObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil mobil1 = new Mobil();
+            Mobil mobil2 = new Mobil();
+            Mobil mobil3 = new Mobil();
+
+            mobil1.Warna         = "Merah";
+            mobil1.JumlahPintu   = "Empat";
+            mobil1.Merek         = "Toyota";
+            mobil1.Model         = "MVP";
+            mobil1.TahunKeluaran = "2022";
+
+
+
+
+
+            mobil1.Input();
+            mobil2.Klakson();
+            mobil3.Info();
+        }
+    }
+}
